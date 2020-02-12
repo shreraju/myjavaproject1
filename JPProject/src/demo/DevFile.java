3cf43fd573ede9f926bc339678ebd3106e36cb81
shreraju <60939415+shreraju@users.noreply.github.com>
2020-02-11 22:22:37 -0500

    Updated devfile with 2

--- JPProject/src/demo/DevFile.java
+++ JPProject/src/demo/DevFile.java
@@ -5,6 +5,7 @@ public class DevFile {
 	public static void main(String[] args) {
 		
 		System.out.println("This is dev env");
+		system.out.println("this is dev 2");
 		// TODO Auto-generated method stub
 
 	}
